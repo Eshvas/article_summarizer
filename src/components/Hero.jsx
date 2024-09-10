156bbbad30b743b99371701752cea52622d7aa4f
Eshvas <98749923+Eshvas@users.noreply.github.com>
2024-09-10 22:02:53 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/Eshvas", "_blank")
+            window.open("https://github.com/Eshvas/article_summarizer", "_blank")
           }
           className='black_btn'
         >
